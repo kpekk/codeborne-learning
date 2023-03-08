1c4e32345d03a04b2d4beb258c99127cc2fb5b0b
tarkvarakool <tarkvarakool@epost.ee>
2023-03-08 15:53:43 +0200

    Fibonacci recursive solution with cache

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Recursion {
 
     public int fibonacciRecursive(int a) {
@@ -9,10 +12,23 @@ public class Recursion {
         return fibonacciRecursive(a - 1) + fibonacciRecursive(a - 2);
     }
 
-    public int fibonacci(int n) {
-        int n1 = 0;
-        int n2 = 1;
-        int n3 = 0;
+    static Map<Integer, Long> cache = new HashMap<>();
+
+    public long fibonacciRecursiveCache(int a) {
+
+        Long precalculated = cache.get(a);
+        if (precalculated != null) {
+            return precalculated;
+        }
+        long result = a < 2 ? a : fibonacciRecursiveCache(a - 1) + fibonacciRecursiveCache(a - 2);
+        cache.put(a, result);
+        return result;
+    }
+
+    public long fibonacci(int n) {
+        long n1 = 0;
+        long n2 = 1;
+        long n3 = 0;
         for (int i = 1; i < n; i++) {
             n3 = n1 + n2;
             n1 = n2;
@@ -23,7 +39,21 @@ public class Recursion {
 
     public static void main(String[] args) {
         Recursion r = new Recursion();
-        r.fibonacciRecursive(5);
+
+        long startTime = System.nanoTime();
+        //System.out.println(r.fibonacciRecursive(40));
+        long endTime = System.nanoTime();
+        //System.out.println("f1: " + (endTime-startTime));
+
+        startTime = System.nanoTime();
+        System.out.println(r.fibonacciRecursiveCache(50));
+        endTime = System.nanoTime();
+        System.out.println("f2: " + (endTime-startTime));
+
+        startTime = System.nanoTime();
+        System.out.println(r.fibonacci(50));
+        endTime = System.nanoTime();
+        System.out.println("f3: " + (endTime-startTime));
     }
 
 }
