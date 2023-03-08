30e8c7c807e748b14ff264269579d93012b5a5ef
tarkvarakool <tarkvarakool@epost.ee>
2023-03-08 12:36:51 +0200

    Fibonacci recursive and iterative solution + tests

---
+++ src/Recursion.java
@@ -0,0 +1,30 @@
+public class Recursion {
+
+    public int fibonacciRecursive(int a) {
+        if (a == 0) {
+            return 0;
+        } else if (a == 1) {
+            return 1;
+        }
+        return fibonacciRecursive(a - 1) + fibonacciRecursive(a - 2);
+    }
+
+    public int fibonacci(int n) {
+        int n1 = 0;
+        int n2 = 1;
+        int n3 = 0;
+        for (int i = 1; i < n; i++) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; ++i) {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ test/RecursionTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RecursionTest {
+
+    // https://en.wikipedia.org/wiki/Factorial
+
+    private final Recursion recursion = new Recursion();
+
+    @Test
+    void fibonacciRecursive0() {
+        assertEquals(0, recursion.fibonacciRecursive(0));
+    }
+
+    @Test
+    void fibonacciRecursive1() {
+        assertEquals(1, recursion.fibonacciRecursive(1));
+    }
+
+    @Test
+    void fibonacciRecursive2() {
+        assertEquals(1, recursion.fibonacciRecursive(2));
+    }
+
+    @Test
+    void fibonacciRecursive13() {
+        assertEquals(233, recursion.fibonacciRecursive(13));
+    }
+
+    @Test
+    void fibonacciRecursive5() {
+        assertEquals(5, recursion.fibonacciRecursive(5));
+    }
+
+    @Test
+    void fibonacciRecursive19() {
+        assertEquals(4181, recursion.fibonacciRecursive(19));
+    }
+
+    @Test
+    void fibonacci2() {
+        assertEquals(1, recursion.fibonacci(2));
+    }
+
+    @Test
+    void fibonacci19() {
+        assertEquals(4181, recursion.fibonacci(19));
+    }
+
+}
